f9baf4862c1f0c3dfa19071430ce2df268805dd6
Mahbub Mozadded <sowrov@gmail.com>
2016-08-18 18:06:47 +0200

    candies

--- 2016/miscellaneous/Mis2016/Mis2016/candies.cpp
+++ 2016/miscellaneous/Mis2016/Mis2016/candies.cpp
@@ -34,25 +34,89 @@ using namespace std;
 
 int r[BN];
 int cand[BN];
+struct Pair {
+    int s, e;
+    char t;
+};
 
+vector<Pair> pList;
 
 int main () {
     int n;
-
+    //pList = new vector<Pair>();
+    pList.clear();
     cin>>n;
-    for (int i=0; i<n; i++) {
+    cin>>r[0];
+    Pair p;
+    p.s=0;
+    p.e=-1;
+    p.t='x';
+    for (int i=1; i<n; i++) {
         cin>>r[i];
+        if (r[i]<r[i-1]) {
+            if(p.t=='i' || p.t=='e') {
+                p.e=i-1;
+                pList.push_back(p);
+                p.s=i-1; //decreasing start from last big number
+                p.e=-1;
+            }
+            p.t='d';
+        } else if (r[i]>r[i-1]){
+            if(p.t=='d' || p.t=='e') {
+                p.e=i-1;
+                pList.push_back(p);
+                p.s=i-1;
+                p.e=-1;
+            }
+            p.t='i';
+        } else {
+            if(p.t=='d' || p.t=='i') {
+                p.e=i-1;
+                pList.push_back(p);
+                p.s=i; //ignore last number
+                p.e=-1;
+            }
+            p.t='e';
+        }
     }
 
-    int k=-1;
-    for (int i=0; i<n; i++) {
-        cand[i] = 1;
-        if (i>0 && r[i] > r[i-1]) {
-            cand[i] =cand[i-1]+1;
-        } else if (i>0 && r[i] < r[i-1]) {
-            cand[i] =cand[i-1]-1;
+    if (p.e==-1) {
+        if (p.t=='x') {
+            p.t='i';
         }
-        
+        p.e=n-1;
+        pList.push_back(p);
     }
+    long long sum=0;
+    int v=0;
+    for (int i=0; i<pList.size(); i++) {
+        if (pList[i].t=='e' || pList[i].t=='i') {
+            if (pList[i].s==0 || pList[i].s==pList[i].e || pList[i].t=='e') {
+                cand[pList[i].s]=1;
+            }
+            for (int j=pList[i].s+1; j<=pList[i].e; j++) {
+                if (pList[i].t=='i') {
+                    cand[j]=cand[j-1]+1;
+                } else {
+                    cand[j]=1;
+                }
+            }
+        } else {
+            v=1;
+            for (int j=pList[i].e; j>pList[i].s; j--) {
+                cand[j]=v++;
+            }
+            if (v>cand[pList[i].s]) {
+                cand[pList[i].s]=v;
+            }
+        }
+    }
+
+    for (int i=0; i<n; i++) {
+        //cout<<cand[i]<<" ";
+        sum+=cand[i];
+    }
+    //cout<<" -- "<<endl;
+    cout<<sum<<endl;
     return 0;
 }
