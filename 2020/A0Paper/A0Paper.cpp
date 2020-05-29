6c2529cc3a13a7a644dba08183622ed6e92a1e16
sowrov <sowrov@gmail.com>
2020-05-29 09:44:06 +0200

    tc A0Paper

---
+++ 2020/A0Paper/A0Paper.cpp
@@ -0,0 +1,224 @@
+#include "cassert"
+#include "iostream"
+#include "numeric"
+#include "bitset"
+#include "deque"
+#include "cstdio"
+#include "algorithm"
+#include "iomanip"
+#include "stack"
+#include "cstring"
+#include "functional"
+#include "cstdlib"
+#include "string"
+#include "vector"
+#include "list"
+#include "set"
+#include "queue"
+#include "map"
+#include "sstream"
+#include "cmath"
+#include "fstream"
+#include "utility"
+#include "ctime"
+#include "cctype"
+
+using namespace std;
+
+#define Zclr(arr) memset(arr, 0, sizeof(arr))
+#define Nclr(arr) memset(arr, -1, sizeof(arr))
+#define M31 1<<30
+#define Pi (2.0*acos(0.0))
+#define Eps (1e-9)
+#define Rep(_var, _N) for (int _var=0; _var<_N; _var++ )
+#define For(_var, _S,  _N) for (int _var=_S; _var<=_N; _var++ )
+
+class A0Paper {
+public:
+    string canBuild(vector<int> A) {
+	for(int i=A.size()-1; i>0; i--) {
+		int nextSize = A[i]/2;
+		A[i-1]+=nextSize;
+	}
+	if (A[0]>=1) {
+		return "Possible";
+	} else {
+		return "Impossible";
+	}
+    }
+};
+
+// <editor-fold defaultstate="collapsed" desc="Generated Code">
+namespace otocoder {
+    int run_test_case(int );
+    bool run_full_test();
+    // casenum<-1 to run full test set, casenum=-1 (default) to run all sample test cases,
+    // casenum>-1 value to run specific sample case
+    void run_test(int casenum = -1, bool quiet = false) {
+        if (casenum < -1) {
+            if (!run_full_test()) {
+                cerr << "Illegal inputs in full test cases!" << endl;
+            }
+            return;
+        }
+        if (casenum > -1) {
+            if (run_test_case(casenum) == -1 && !quiet) {
+                cerr << "Illegal input! Test case " << casenum << " does not exist." << endl;
+            }
+            return;
+        }
+
+        int correct = 0, total = 0;
+        for (int i=0;; ++i) {
+            int x = run_test_case(i);
+            if (x == -1) {
+                if (i >= 100) break;
+                continue;
+            }
+            correct += x;
+            ++total;
+        }
+
+        if (total == 0) {
+            cerr << "No test cases run." << endl;
+        } else if (correct < total) {
+            cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << endl;
+        } else {
+            cerr << "All " << total << " tests passed!" << endl;
+        }
+    }
+
+    int verify_case(int casenum, const string &expected, const string &received, clock_t elapsed) { 
+        cerr << "Example " << casenum << "... "; 
+
+        string verdict;
+        vector<string> info;
+        char buf[100];
+
+        if (elapsed > CLOCKS_PER_SEC / 200) {
+            sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+            info.push_back(buf);
+        }
+
+        if (expected == received) {
+            verdict = "PASSED";
+        } else {
+            verdict = "FAILED";
+        }
+
+        cerr << verdict;
+        if (!info.empty()) {
+            cerr << " (";
+            for (int i=0; i<(int)info.size(); ++i) {
+                if (i > 0) cerr << ", ";
+                cerr << info[i];
+            }
+            cerr << ")";
+        }
+        cerr << endl;
+
+        if (verdict == "FAILED") {
+            cerr << "    Expected: \"" << expected << "\"" << endl;
+            cerr << "    Received: \"" << received << "\"" << endl;
+        }
+
+        return verdict == "PASSED";
+    }
+
+    int run_test_case(int casenum) {
+        switch (casenum) {
+        case 0: {
+            int A[] = {0, 3};
+            string expected_ = "Possible";
+
+            clock_t start_           = clock();
+            string received_ = A0Paper().canBuild(vector<int>(A, A + (sizeof A / sizeof A[0])));
+            return verify_case(casenum, expected_, received_, clock()-start_);
+        }
+        case 1: {
+            int A[] = {0, 1, 2};
+            string expected_ = "Possible";
+
+            clock_t start_           = clock();
+            string received_ = A0Paper().canBuild(vector<int>(A, A + (sizeof A / sizeof A[0])));
+            return verify_case(casenum, expected_, received_, clock()-start_);
+        }
+        case 2: {
+            int A[] = {0, 0, 0, 0, 15};
+            string expected_ = "Impossible";
+
+            clock_t start_           = clock();
+            string received_ = A0Paper().canBuild(vector<int>(A, A + (sizeof A / sizeof A[0])));
+            return verify_case(casenum, expected_, received_, clock()-start_);
+        }
+        case 3: {
+            int A[] = {2, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 5, 0, 3, 0, 0, 1, 0, 0, 0, 5};
+            string expected_ = "Possible";
+
+            clock_t start_           = clock();
+            string received_ = A0Paper().canBuild(vector<int>(A, A + (sizeof A / sizeof A[0])));
+            return verify_case(casenum, expected_, received_, clock()-start_);
+        }
+        default:
+            return -1;
+        }
+    }
+
+    const std::string currentDateTime() {
+        time_t     now = time(0);
+        struct tm  tstruct;
+        char       buf[80];
+        tstruct = *localtime(&now);
+        strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
+        return buf;
+    }
+
+    //This method will only accept input files that are formatted in proper way.
+    //Command line example: A0Paper.exe -2 <A0Paper.io
+    bool run_full_test() {
+        int testCase, N, caseNo, correct=0;
+        vector<int> A;
+        int A_sin;
+        string expected_;
+
+        std::cin>>testCase; std::cin.ignore(10, '\n');
+        for (int i=0; i<testCase; i++) {
+            std::cin>>caseNo; std::cin.ignore(10, '\n');
+            A.clear();
+            std::cin>>N; std::cin.ignore(10, '\n');
+            for(int j=0; j<N; j++) {
+                std::cin>>A_sin; std::cin.ignore(10, '\n');
+                A.push_back(A_sin);
+            }
+            std::getline(std::cin,expected_);
+            clock_t start_           = clock();
+            string received_ = A0Paper().canBuild(A);
+            correct += verify_case(caseNo, expected_, received_, clock()-start_);
+        }//end of testCase for loop
+
+        if (testCase == 0) {
+            cerr << "No test cases run." << endl;
+        } else if (correct < testCase) {
+            cerr << "Some cases FAILED (passed " << correct << " of " << testCase << ")." << endl;
+        } else {
+            cerr << "All " << testCase << " tests passed!" << endl;
+            std::ofstream ofs("A0Paper.st", std::ios_base::app);
+            ofs<<currentDateTime()<<std::endl;
+            ofs.close();
+        }
+        return true;
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+	std::cout<<"Running all\n";
+        otocoder::run_test();
+    } else {
+	std::cout<<"running cases\n ";
+        for (int i=1; i<argc; ++i)
+            otocoder::run_test(atoi(argv[i]));
+    }
+}
+// </editor-fold>
